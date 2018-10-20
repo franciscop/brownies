d1326ff3f1c0a3170f96ebdc839a09868bc84b54
Francisco Presencia <public@francisco.io>
2018-10-20 19:54:21 +0900

    Testing cross subscriptions. Seem to work just fine

--- brownies.test.js
+++ brownies.test.js
@@ -1,5 +1,6 @@
-import { cookies, local } from './brownies.min';
+import { cookies, local, session, subscribe } from './brownies';
 const brownies = require('./brownies.min.js');
+const delay = require('delay');
 const { cookies: cookiesReq, local: localReq } = require('./brownies.min.js');
 
 describe('minimized', () => {
@@ -12,5 +13,21 @@ describe('minimized', () => {
     expect(brownies.local).toBeDefined();
     expect(cookiesReq).toBeDefined();
     expect(localReq).toBeDefined();
+  });
+
+  it('will call it appropriately', () => {
+    const fn = jest.fn();
+    subscribe(cookies, 'same', fn);
+    cookies.same = 'should not trigger';
+    expect(fn).toBeCalledWith('should not trigger', null);
+  });
+
+  it('does not cross-subscribe', async () => {
+    const fn = jest.fn();
+    subscribe(cookies, 'cross', fn);
+    session.cross = 'should not trigger';
+    local.cross = 'should not trigger';
+    await delay(300);
+    expect(fn).not.toBeCalled();
   });
 });
